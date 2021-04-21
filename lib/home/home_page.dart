4cf1d7a7b4299c0ad5a8749736afb6238b1ae79b
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-21 14:23:59 -0400

    Aula 03

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -15,10 +15,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final controller = HomeController();
+
   @override
   void initState() {
     super.initState();
-
     controller.getUser();
     controller.getQuizzes();
     controller.stateNotifier.addListener(() {
@@ -30,39 +30,48 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     if (controller.state == HomeState.success) {
       return Scaffold(
-        appBar: AppBarWidget(
-          user: controller.user!,
-        ),
+        appBar: AppBarWidget(user: controller.user!),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 5),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  LevelButtonWidget(label: 'Fácil'),
-                  LevelButtonWidget(label: 'Médio'),
-                  LevelButtonWidget(label: 'Difícil'),
-                  LevelButtonWidget(label: 'Perito'),
-                ],
+              SizedBox(height: 24),
+              Container(
+                height: 30,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    LevelButtonWidget(
+                      label: "Fácil",
+                    ),
+                    LevelButtonWidget(
+                      label: "Médio",
+                    ),
+                    LevelButtonWidget(
+                      label: "Difícil",
+                    ),
+                    LevelButtonWidget(
+                      label: "Perito",
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(
-                height: 24,
-              ),
+              SizedBox(height: 24),
               Expanded(
-                  child: GridView.count(
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
-                crossAxisCount: 2,
-                children: controller.quizzes!
-                    .map((e) => QuizCardWidget(
-                          title: e.title,
-                          completed:
-                              "${e.questionAnswered}/${e.questions.length}",
-                          percent: e.questionAnswered / e.questions.length,
-                        ))
-                    .toList(),
-              ))
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  children: controller.quizzes!
+                      .map((e) => QuizCardWidget(
+                            title: e.title,
+                            completed:
+                                "${e.questionAnswered}/${e.questions.length}",
+                            percent: e.questionAnswered / e.questions.length,
+                          ))
+                      .toList(),
+                ),
+              ),
             ],
           ),
         ),
--- lib/shared/models/question_model.dart
+++ lib/shared/models/question_model.dart
@@ -1,10 +1,35 @@
+import 'dart:convert';
+
 import 'package:DevQuiz/shared/models/awnser_model.dart';
 
 class QuestionModel {
   final String title;
-  final List<AwnserModel> awnsers;
+  final List<AwnserModel> answers;
+
   QuestionModel({
     required this.title,
-    required this.awnsers,
-  }) : assert(awnsers.length == 4);
+    required this.answers,
+  }) : assert(answers.length == 4);
+  //A qtd de respostas deve ser 4 para garantir a
+  //integridade do layout que tem 4 componentes de respostas
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'answers': answers.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory QuestionModel.fromMap(Map<String, dynamic> map) {
+    return QuestionModel(
+      title: map['title'],
+      answers: List<AwnserModel>.from(
+          map['answers'].map((x) => AwnserModel.fromMap(x))),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuestionModel.fromJson(String source) =>
+      QuestionModel.fromMap(json.decode(source));
 }
