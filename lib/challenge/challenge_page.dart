7efc77e4003092ebe6f43cdb34d230908722a0ec
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-21 08:58:06 -0400

    feat: create awnserWidget

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatefulWidget {
@@ -14,6 +15,7 @@ class _ChallengePageState extends State<ChallengePage> {
         preferredSize: Size.fromHeight(60),
         child: SafeArea(top: true, child: QuestionIndicatorWidget()),
       ),
+      body: QuizWidget(title: 'O que o Flutter faz em sua totalidade?'),
     );
   }
 }
