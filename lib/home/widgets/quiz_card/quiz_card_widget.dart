2a2d9e490b1289e23ba695fe8158c18911d9a201
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-20 20:00:47 -0400

    feat: create ProgressIndicatorWidget

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/core/app_images.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -37,11 +38,7 @@ class QuizCardWidget extends StatelessWidget {
                   flex: 1, child: Text("3/10", style: AppTextStyles.body11)),
               Expanded(
                 flex: 3,
-                child: LinearProgressIndicator(
-                    value: 0.3,
-                    backgroundColor: AppColors.chartSecondary,
-                    valueColor:
-                        AlwaysStoppedAnimation<Color>(AppColors.chartPrimary)),
+                child: ProgressIndicatorWidget(value: 0.3,),
               )
             ],
           )
